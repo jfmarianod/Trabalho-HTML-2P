2d7233d630e3001b8462f68a521419d067c482c8
jfmarianod <jfmarianod@gmail.com>
2021-11-26 22:14:33 -0300

    Primeiro Commit

---
+++ script.js
@@ -0,0 +1,3 @@
+document.querySelector(".hamburguer").addEventListener("click", () =>/* adciona um evento que aguarda a um click para executar uma função*/
+    document.querySelector(".container").classList.toggle("show-menu")/*cria a animação do menu (tudo q estiver dentro do container)*/
+);
